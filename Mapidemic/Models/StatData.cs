8bcb53fe928ea4e1517d48208511087bdb5ff055
Neng Yang <yangne53@uwosh.edu>
2025-11-11 16:57:29 -0600

    Made the illness  name displayed as lowercase when displayed in the list

--- Mapidemic/Models/StatData.cs
+++ Mapidemic/Models/StatData.cs
@@ -4,5 +4,5 @@ public class StatData
 {
     public string Name { get; set; } = "";
     public int Count { get; set; }
-    public string ReportCount => $"# of user(s) reported of {Name} in your area: ";
+    public string ReportCount => $"Number of user(s) reported of {Name.ToLower()} in your area: ";
 }
