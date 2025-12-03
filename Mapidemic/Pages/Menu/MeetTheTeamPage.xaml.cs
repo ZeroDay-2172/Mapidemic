9c6ab07697670dce87898ab06bbe8698f67f76b1
Arthur Waldman <Waldma51@uwosh.edu>
2025-12-03 17:43:44 -0600

    menu pages alerts are now toast. Added comments to menu pages

--- Mapidemic/Pages/Menu/MeetTheTeamPage.xaml.cs
+++ Mapidemic/Pages/Menu/MeetTheTeamPage.xaml.cs
@@ -1,10 +1,15 @@
 namespace Mapidemic.Pages.Menu;
 
+/// <summary>
+/// A class that provides a user interface for displaying information on the Mapidemic team
+/// </summary>
 public partial class MeetTheTeamPage : TabbedPage
 {
+    /// <summary>
+    /// The default constructor for a MeetTheTeamPage
+    /// </summary>
     public MeetTheTeamPage()
     {
         InitializeComponent();
     }
-    
 }
