1a7a9f4c122e1640e741f2ea4e0022613e2c3934
Neng Yang <yangne53@uwosh.edu>
2025-12-04 12:19:37 -0600

    Update MeetTheTeam page for Neng and update to use syncfusion expander on PrivacyPolicy and InformationWeCollect

--- Mapidemic/Pages/Menu/MeetTheTeamPage.xaml.cs
+++ Mapidemic/Pages/Menu/MeetTheTeamPage.xaml.cs
@@ -12,4 +12,39 @@ public partial class MeetTheTeamPage : TabbedPage
     {
         InitializeComponent();
     }
+
+    /// <summary>
+    /// Opens the default email client to contact Neng Yang
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void NengEmailClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            await Launcher.OpenAsync("mailto:yangne53@uwosh.edu");
+        }
+        catch (Exception)
+        {
+            await DisplayAlert("Error", "Unable to open email client.", "OK");
+        }
+    }
+
+    /// <summary>
+    /// Opens Neng Yang's LinkedIn profile in the default web browser
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void NengLinkedInClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var uri = new Uri("https://www.linkedin.com/in/nengyang93/");
+            await Launcher.OpenAsync(uri);
+        }
+        catch (Exception)
+        {
+            await DisplayAlert("Error", "Unable to open LinkedIn profile.", "OK");
+        }
+    }
 }
