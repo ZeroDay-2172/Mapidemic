b9a9b0bf77b206f9a979f7719688dc8be53f6342
Arthur Waldman <Waldma51@uwosh.edu>
2025-12-09 17:05:30 -0600

    added comments to the settings class

--- Mapidemic/Models/Settings.cs
+++ Mapidemic/Models/Settings.cs
@@ -8,6 +8,11 @@ public class Settings
     public AppTheme Theme { get; set; }
     public int PostalCode { get; set; }
 
+    /// <summary>
+    /// The designated constructor for a Settings object
+    /// </summary>
+    /// <param name="theme"></param>
+    /// <param name="postalCode"></param>
     public Settings(AppTheme theme, int postalCode)
     {
         Theme = theme;
