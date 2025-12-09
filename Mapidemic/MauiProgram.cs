b24ecc81f79e99bf16df741291f86aff0bbfbd83
Arthur Waldman <Waldma51@uwosh.edu>
2025-12-09 17:13:03 -0600

    added final comment to MauiProgram

--- Mapidemic/MauiProgram.cs
+++ Mapidemic/MauiProgram.cs
@@ -16,6 +16,11 @@ public static class MauiProgram
 {
 	private const string sfKey = "Ngo9BigBOggjGyl/Vkd+XU9FcVRDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS3tSdkRiWH9dcHFRT2ZbVU91Xg==";
 	public static BusinessLogic businessLogic = new BusinessLogic(new Database());
+
+	/// <summary>
+    /// A function that creates Mapidemic
+    /// </summary>
+    /// <returns>Mapidemic</returns>
 	public static MauiApp CreateMauiApp()
 	{
 		SyncfusionLicenseProvider.RegisterLicense(sfKey);
